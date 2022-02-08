51fa063ddb8f8d26d0ea56501c60f550637d2f18
Alex Gill-Boucher <arg5571@ad.rit.edu>
2022-02-08 17:18:23 -0500

    built api, should be working. begun improving client functionality

---
+++ src/jsonResponses.js
@@ -0,0 +1,61 @@
+const users = {};
+
+const respondJSON = (request, response, status, obj) => {
+  const jsonString = JSON.stringify(obj);
+
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+
+  if (request.method !== 'HEAD') {
+    response.write(jsonString);
+  }
+
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  // here i think i ought to pass in {users} and not users
+  // i believe prof specifically addressed this in a demo, but i cant remember the reason
+  // ill have to test and see whats different once the server is working
+  respondJSON(request, response, 200, users);
+};
+
+const notFound = (request, response) => {
+  const errorObj = {
+    message: 'the page you were looking for couldnt be found',
+    id: 'notFound',
+  };
+
+  respondJSON(request, response, 404, errorObj);
+};
+
+const addUser = (request, response, body) => {
+  if (!body.name || !body.age) {
+    const errorObj = {
+      message: 'a user must have both a name and an age',
+      id: 'missingParams',
+    };
+
+    respondJSON(request, response, 400, errorObj);
+  }
+
+  const resObj = { message: 'user info updated' };
+  let status = 204;
+
+  if (!users[body.name]) {
+    users[body.name] = {};
+
+    resObj.message = 'new user created';
+    status = 201;
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  respondJSON(request, response, status, resObj);
+};
+
+module.exports = {
+  getUsers,
+  notFound,
+  addUser,
+};
---
+++ src/server.js
@@ -0,0 +1,71 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const gettableURLs = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getStyle,
+  '/getUsers': jsonHandler.getUsers,
+  '/notFound': jsonHandler.notFound,
+};
+
+const postableURLs = {
+  '/addUser': jsonHandler.addUser,
+};
+
+const handlePost = (request, response, parsedURL) => {
+  if (!postableURLs[parsedURL.pathname]) {
+    jsonHandler.notFound(request, response);
+    return;
+  }
+
+  const body = [];
+
+  request.on('error', (err) => {
+    console.log(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    postableURLs[parsedURL.pathname](request, response, bodyParams);
+  });
+};
+
+const onRequest = (request, response) => {
+  const parsedURL = url.parse(request.url);
+    
+  console.log(parsedURL.pathname);
+    console.log(request.method);
+    
+  switch (request.method) {
+    case 'GET': // I should be able to use the same methods for get and head requests, excluding the body of the response if its a head
+    case 'HEAD': // so I'll have get fall through to head to execute the same code
+      if (gettableURLs[parsedURL.pathname]) {
+        gettableURLs[parsedURL.pathname](request, response);
+        break;
+      }
+      jsonHandler.notFound(request, response);
+      break;
+    case 'POST':
+      handlePost(request, response, parsedURL);
+      break;
+    default:
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`server is listening to 127.0.0.1:${port}`);
+});
